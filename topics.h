27a1ddf6d418b9556c7eea6b62ae125a4bd143aa
sonu99 <sks993463@gmail.com>
2016-08-06 14:30:57 +0530

    topics for discussion

---
+++ topics.h
@@ -0,0 +1,20 @@
+1> Programming
+      -> C
+      -> C++
+      -> Assembly (ARM)
+ 2> Platform specific Programming
+        -> Linux system programming
+ 3> Linux
+         -> linux internal
+         -> linux kernel programming
+         -> Linux device driver (char, PCI, Parllel, USB, Block)
+         -> kernel level semaphore, mutex, memory management, memory request.
+---------------------------------------------------------------------
+4> Board bring-up
+         -> kernel customization and porting
+         -> u-boot customization and porting
+         -> root file system build
+5> hardware and protocol
+           -> I2C, SPI, UART, USB, BT
+           -> Bigglebone, pi board
+6> ARM architecture 
